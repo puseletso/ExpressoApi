81f5c5b94601e9ce2b29618a22ff2cf9c308c9c4
puseletso <puseletsomaraba@gmail.com>
2021-09-10 19:06:07 +0200

    first commit

---
+++ ExpressoApi/Pages/MenuPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpressoApi.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Menu = ExpressoApi.Models.Menu;
+
+namespace ExpressoApi.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPage : ContentPage
+    {
+
+        //contains the full list
+        public ObservableCollection<Menu> Menus;
+
+        public MenuPage()
+        {
+            InitializeComponent();
+            Menus = new ObservableCollection<Menu>();
+
+        }
+        //function will return list of menu
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            ApiServices apiServices = new ApiServices();
+            var menus = await apiServices.GetMenu();
+
+            //to show the full list one by one we use the foreach loop
+            foreach (var menu in menus)
+            {
+                Menus.Add(menu);
+                
+            }
+
+            LvMenu.ItemsSource = Menus;
+
+        }
+    }
+}
---
+++ ExpressoApi/Services/ApiServices.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using ExpressoApi.Models;
+using Newtonsoft.Json;
+
+namespace ExpressoApi.Services
+{
+    public class ApiServices
+    {
+        public async Task<List<Menu>> GetMenu()
+        {
+            var client = new HttpClient();
+            var response = await client.GetStringAsync("https://expressoapipj.azurewebsites.net/api/menus");
+            return JsonConvert.DeserializeObject<List<Menu>>(response);
+
+        }
+
+        public async Task<bool> ReserveTable(Reservation reservation)
+        {
+            var client = new HttpClient();
+            var json = JsonConvert.SerializeObject(reservation);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://expressoapipj.azurewebsites.net/api/reservations", content);
+            return response.IsSuccessStatusCode;
+
+        }
+    }
+}
